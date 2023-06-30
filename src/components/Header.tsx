dd920b7063199d90233b1b848e006eb5745f27e4
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2023-06-30 07:58:44 +0300

    feat: init Redux toolkit

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+'use strict';
+
+import React from 'react';
+import { Animated, StyleSheet, View } from 'react-native';
+import { useBounceAnimation, useViewportUnits } from '../hooks';
+
+const Header = () => {
+  const { vh } = useViewportUnits();
+  const bounce = useBounceAnimation();
+  const height = 40 * vh;
+
+  return (
+    <View style={styles.container}>
+      <Animated.Image
+        accessibilityRole={'image'}
+        source={require('./logo.gif')}
+        style={{ height, transform: [{ translateY: bounce }] }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+});
+export default Header;
