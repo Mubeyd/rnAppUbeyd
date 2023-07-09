da0c15f53e113df11a781f18c12efa35f197cdd7
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2023-07-09 16:00:56 +0300

    refactor: remove details screen

--- src/navigation/HomeStack.tsx
+++ src/navigation/HomeStack.tsx
@@ -1,6 +1,5 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import DetailsScreen from '../screens/DetailsScreen';
 import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createStackNavigator();
@@ -13,13 +12,6 @@ export function HomeStack() {
         component={HomeScreen}
         options={{
           title: 'Home Screen RQ',
-        }}
-      />
-      <Stack.Screen
-        name="DetailsScreen"
-        component={DetailsScreen}
-        options={{
-          title: 'Details Screen',
         }}
       />
     </Stack.Navigator>
