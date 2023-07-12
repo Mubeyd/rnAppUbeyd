3f050651b32d62cb0e793ef84baa166a22072bd4
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2023-07-12 13:40:07 +0300

    feat: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: 'react-native',
   setupFiles: ['<rootDir>/jest/setup.js'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$':
+      '<rootDir>/node_modules/react-native/Libraries/Image/RelativeImageStub',
+  },
+  collectCoverage: true,
 };
